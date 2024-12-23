9c67ee000f880db934942592709d7664a8d2a412
Niharrakholiya <nihar.rakholiya526@gmail.com>
2024-12-23 09:36:46 +0530

    first commit

---
+++ counter/src/App.tsx
@@ -0,0 +1,15 @@
+import Count from './components/Count'
+
+function App() {
+
+  return (
+    <>
+    <button>Increment</button>
+    <Count />
+    <button>Decrement</button>
+
+    </>
+  )
+}
+
+export default App
---
+++ counter/src/components/Count.tsx
@@ -0,0 +1,6 @@
+function Count(){
+    return(
+        <h1>0</h1>
+    )
+}
+export default Count;
